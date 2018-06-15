252aa211c39710b2044fb96f012e989efbe3536a
Bjornskjald <github@bjorn.ml>
2018-06-16 00:06:12 +0200

    Changed "image" to array of attachments

--- src/models.ts
+++ src/models.ts
@@ -2,7 +2,7 @@ import { Observable, Subject } from 'rxjs'
 
 export interface IFacegramMessage {
   message: string
-  image: string
+  attachments: { url: string, name: string }[]
   author: string
   origin: IFacegramService
   serviceConnection: IFacegramServiceConnection
