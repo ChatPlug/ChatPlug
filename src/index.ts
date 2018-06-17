0782cb2b979022159334e7ab3b7a90cb76b70d9f
Bjornskjald <github@bjorn.ml>
2018-06-17 19:33:35 +0200

    Added "logging out" message

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { Facegram } from './Facegram'
 const facegram = new Facegram()
 facegram.startBridge().then().catch()
 
-process.on('SIGINT', () => facegram.stopBridge()
-  .then(() => process.exit(log.info('', 'Logged out') || 0))
-  .catch(err => process.exit(log.error('', err) || 1)))
+process.on('SIGINT', () => {
+  log.info('', 'Logging out...')
+  facegram.stopBridge().then(() => process.exit(log.info('', 'Logged out') || 0)).catch(err => process.exit(log.error('', err) || 1))
+})
