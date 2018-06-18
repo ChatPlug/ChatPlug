fc9225c2a0b7e515e96c4d5ac5462c107ea2b75b
Bjornskjald <github@bjorn.ml>
2018-06-18 23:19:46 +0200

    Added Facebook message handling

--- src/services/facebook/FacebookService.ts
+++ src/services/facebook/FacebookService.ts
@@ -1,5 +1,5 @@
 import log from 'npmlog'
-import { IFacegramMessage } from '../../models'
+import { IFacegramMessage, IFacegramThread } from '../../models'
 import { FacegramService } from '../Service'
 import { Subject } from 'rxjs'
 import { FacebookConfig } from './FacebookConfig'
@@ -7,6 +7,8 @@ import facebook from 'facebook-chat-api'
 import { createInterface } from 'readline'
 import { ExchangeManager } from '../../ExchangeManager'
 import { ThreadConnectionsManager } from '../../ThreadConnectionsManager'
+import { promisify } from 'util'
+import { parse } from 'url'
 
 const rl = createInterface({
   input: process.stdin,
@@ -20,6 +22,8 @@ export default class FacebookService implements FacegramService {
   receiveMessageSubject: Subject<IFacegramMessage> = new Subject()
   config: FacebookConfig
   facebook: any
+  stopListening: any
+  handledMessages: any[] = []
 
   constructor(config: FacebookConfig, exchangeManager: ExchangeManager, threadConnectionsManager: ThreadConnectionsManager) {
     this.messageSubject = exchangeManager.messageSubject
@@ -27,7 +31,83 @@ export default class FacebookService implements FacegramService {
     this.isEnabled = config.enabled
   }
 
-  initialize() {
+  async initialize () {
+    await this.login()
+
+    this.receiveMessageSubject.subscribe(async message => {
+      if (!message.target) return
+      this.facebook.sendMessage(
+        {
+          body: `*${message.author.username}*: ${message.message}`,
+          attachment: await Promise.all(message.attachments.map(attach => attach.url).map(getStreamFromURL)),
+        },
+        message.target.id,
+        err => { if (err) log.error('facebook', err) },
+      )
+    })
+
+    this.stopListening = this.facebook.listen(this.listener)
+  }
+
+  listener = async (err, message) => {
+    if (err) return log.error('facebook', err)
+
+    // Reconnecting
+    if (!message) {
+      log.warn('facebook', 'Message is undefined/null, internet connection may be unavailable')
+      log.info('facebook', 'Reconnecting...')
+      this.stopListening()
+      this.login().then(() => {
+        log.info('facebook', 'Reconnected to Facebook')
+        this.stopListening = this.facebook.listen(this.listener)
+      })
+    }
+
+    // Duplicates handling
+    if (this.handledMessages.includes(message.messageID)) return log.verbose('facebook', 'Possible duplicate message, ignoring')
+    if (this.handledMessages.length > 100) this.handledMessages.splice(100)
+    this.handledMessages.push(message.messageID)
+
+    // TODO: add logging to this part of the script
+    const thread = await promisify(this.facebook.getThreadInfo)(message.threadID)
+    const sender = (await promisify(this.facebook.getUserInfo)(message.senderID))[message.senderID]
+
+    const facegramMessage = {
+      message: message.body,
+      attachments: message.attachments.map(attach => {
+        if (attach.type === 'share') return // TODO: parse share attachments correctly
+
+        let url = attach.image || attach.url
+        if (!url) return // failsafe, but it shouldn't happen
+
+        if (url.match(/^(http|https):\/\/l\.facebook\.com\/l\.php/i)) {
+          url = parse(url, true).query.u
+        }
+
+        if (parse(url).pathname === '/safe_image.php') {
+          url = parse(url, true).query.url
+        }
+
+        return {
+          url,
+          name: (parse(url).pathname || 'filename').split('/').pop(),
+        }
+      }).filter(x => x),
+      author: {
+        username: thread.nicknames[message.senderID] || sender.name,
+        avatar: `https://graph.facebook.com/${message.senderID}/picture?width=128`,
+        id: message.senderID,
+      },
+      origin: {
+        id: thread.threadID,
+        service: this.name,
+      },
+    } as IFacegramMessage
+
+    this.messageSubject.next(facegramMessage)
+  }
+
+  login () {
     return new Promise((resolve, reject) => {
       facebook(
         {
@@ -59,4 +139,8 @@ export default class FacebookService implements FacegramService {
       this.facebook.logout(err => (err ? reject(err) : resolve())),
     )
   }
+}
+
+function getStreamFromURL(url) {
+  return new Promise((resolve, reject) => require('https').get(url, res => resolve(res)))
 }
