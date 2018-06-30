2bb86226ce1b48a7a05897d237fe2535e007570d
Bjornskjald <github@bjorn.ml>
2018-06-30 19:41:56 +0200

    Config TOML refactor

--- src/ChatPlugConfig.ts
+++ src/ChatPlugConfig.ts
@@ -1,81 +1,92 @@
 import fs = require('fs')
 import log from 'npmlog'
+import path from 'path'
+import TOML from '@iarna/toml'
 import { IChatPlugConnection } from './models'
 
-const CONFIG_FILE_PATH = './config.json'
+const CONFIG_FOLDER_PATH = path.join(__dirname, 'config')
 
-const DEFAULT_CONFIG = {
-  loadedServices: [],
-  useFallbackService: false,
-  fallbackService: 'grpc',
-  services: {
-    telegram: {
-      enabled: false,
-      botToken: 'bot token from botfather',
-    },
-
-    telegramThreadImporter: {
-      apiId: 'your apiId',
-      apiHash: 'your api hash',
-      phoneNumber: 'your telegram phone number',
-    },
-
-    facebook: {
-      enabled: false,
-      email: 'account email',
-      password: 'account password',
-      forceLogin: true,
-    },
-
-    discord: {
-      enabled: false,
-      token: 'discord bot token',
-    },
-
-    grpc: {
-      enabled: false,
-      port: 5319,
-    },
+const DEFAULTS = {
+  core: {
+    loadedServices: [],
+    useFallbackService: false,
+    fallbackService: 'grpc',
+    serviceConnections: [],
+    logLevel: 'info',
   },
-
-  serviceConnections: [],
-
-  logLevel: 'info',
+  telegram: {
+    enabled: false,
+    botToken: 'bot token from BotFather',
+  },
+  telegramThreadImporter: {
+    apiId: 'account API ID',
+    apiHash: 'account API hash',
+    phoneNumber: 'account phone number',
+  },
+  facebook: {
+    enabled: false,
+    email: 'account email',
+    password: 'account password',
+    forceLogin: false,
+  },
+  discord: {
+    enabled: false,
+    token: 'discord bot token',
+  },
+  grpc: {
+    enabled: false,
+    port: 5319,
+  },
 }
+
 export class ChatPlugConfig {
-  jsonConfig: any
+  tomlConfig: any
   constructor () {
-    // If config file doesn't exist, create one
-    if (!fs.existsSync(CONFIG_FILE_PATH)) {
-      this.writeConfig(DEFAULT_CONFIG)
-      log.info('config', 'Written default config to ' + CONFIG_FILE_PATH)
-      process.exit(0)
+    // If config folder doesn't exist, create one
+    if (!fs.existsSync(CONFIG_FOLDER_PATH)) {
+      fs.mkdirSync(CONFIG_FOLDER_PATH)
     }
+    // Check for every config file if exists, write default if doesn't
+    Object.keys(DEFAULTS).forEach(configName => {
+      const configPath = path.join(CONFIG_FOLDER_PATH, configName + '.toml')
+      if (!fs.existsSync(configPath)) {
+        log.info('config', `Writing default ${configName} config to ${configPath}`)
+        fs.writeFileSync(configPath, DEFAULTS[configName])
+      }
+    })
 
     // Read config
-    this.jsonConfig = JSON.parse(fs.readFileSync(CONFIG_FILE_PATH, 'utf-8'))
-    if (this.jsonConfig.logLevel) log.level = this.jsonConfig.logLevel
-    if (process.env.LOG_LEVEL) log.level = process.env.LOG_LEVEL
+    this.tomlConfig = {}
+    Object.keys(DEFAULTS).forEach(service => {
+      this.tomlConfig[service] = this.readConfig(service)
+    })
+    log.level = process.env.LOG_LEVEL || this.tomlConfig.core.logLevel || 'info'
   }
 
   addThreadConnection(threadConnection: IChatPlugConnection) {
-    this.jsonConfig.serviceConnections.push(threadConnection)
-    this.writeConfig(this.jsonConfig)
+    this.tomlConfig.core.serviceConnections.push(threadConnection)
+    this.writeConfig(this.tomlConfig)
   }
 
   getConfigForServiceName (name: string) {
-    return this.jsonConfig['services'][name]
+    return this.tomlConfig[name]
   }
 
   getLoadedServices(): string[] {
-    return this.jsonConfig.loadedServices
+    return this.tomlConfig.core.loadedServices
   }
 
   getThreadConnections (): IChatPlugConnection[] {
-    return this.jsonConfig['serviceConnections']
+    return this.tomlConfig.core.serviceConnections
   }
 
   writeConfig (config: any) {
-    fs.writeFileSync(CONFIG_FILE_PATH, JSON.stringify(config, null, 2))
+    Object.entries(config).forEach(([serviceName, serviceConfig]) => {
+      fs.writeFileSync(path.join(CONFIG_FOLDER_PATH, serviceName + '.toml'), TOML.stringify(serviceConfig))
+    })
+  }
+
+  readConfig (name: string) {
+    return TOML.parse(fs.readFileSync(path.join(CONFIG_FOLDER_PATH, name + '.toml')))
   }
 }
