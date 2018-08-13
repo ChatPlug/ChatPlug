7f3b8c277ac0fade97e127f6dd8e1bf6673dc8ff
alufers <alufers@wp.pl>
2018-08-13 10:13:54 +0200

    Preparing dashboard service

---
+++ src/services/dashboard/DashboardService.ts
@@ -0,0 +1,12 @@
+import { ChatPlugService } from '../Service'
+import DashboardConfig from './DashboardConfig';
+
+export default class ApiService extends ChatPlugService<DashboardConfig> {
+  async initialize() {
+      if(!this.config.bindToAPIServer) {
+          throw new Error("Standalone dashboard server not yet implemented")
+      }
+  }
+
+  async terminate() {}
+}
