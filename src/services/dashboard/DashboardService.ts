30d5d5cfe35d44e7e5fc3bf1f785ee1b8aa4b95e
alufers <alufers@wp.pl>
2018-08-17 21:54:48 +0200

    Added vue to dashboard service

--- src/services/dashboard/DashboardService.ts
+++ src/services/dashboard/DashboardService.ts
@@ -1,11 +1,28 @@
 import { ChatPlugService } from '../Service'
 import DashboardConfig from './DashboardConfig'
+import Service from '../../entity/Service'
+import ApiService from '../api/ApiService'
+import dashboardHttpHandler from './web/dashboardHttpHandler'
 
-export default class ApiService extends ChatPlugService<DashboardConfig> {
+export default class DashboardService extends ChatPlugService<DashboardConfig> {
+  async getAPIExpressInstance() {
+    const dataFromDB = (await this.context.connection
+      .getRepository(Service)
+      .find({ where: { moduleName: 'api', enabled: true } }))[0]
+    if (!dataFromDB) {
+      throw new Error('API service not found!')
+    }
+    const apiService = this.context.serviceManager.getServiceForId(
+      dataFromDB.id as any,
+    ) as ApiService
+    return apiService.app
+  }
   async initialize() {
     if (!this.config.bindToAPIServer) {
       throw new Error('Standalone dashboard server not yet implemented')
     }
+    const app = await this.getAPIExpressInstance()
+    app.use('*', dashboardHttpHandler())
   }
 
   async terminate() {}
---
+++ src/services/dashboard/web/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h1 class="header">ChatPlug dashboard</h1>
+  </section>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue
+} from "nuxt-property-decorator"
+@Component({
+
+})
+export default class extends Vue {
+  async asyncData() {
+    return {
+      
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
