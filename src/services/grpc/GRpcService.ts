c04b7f05ebb3f41d87e8837537503dbbe9457922
Filip Grzywok <rj23@protonmail.com>
2018-08-20 12:44:31 +0200

    downgrade ts-loader

--- src/services/grpc/GRpcService.ts
+++ src/services/grpc/GRpcService.ts
@@ -5,18 +5,12 @@ import { Subject } from 'rxjs'
 import GRpcConfig from './GRpcConfig'
 import { ExchangeManager } from '../../ExchangeManager'
 import { ThreadConnectionsManager } from '../../ThreadConnectionsManager'
-import * as grpc from 'grpc'
 import { ConnectionsService } from './ConnectionsService'
 import { ChatPlugConfig } from '../../ChatPlugConfig'
 import { MessagesService } from './MessagesService'
 
-const CONNECTIONS_PROTO_PATH = __dirname + '/../../protos/connections.proto'
-
-const connectionsProto = grpc.load(CONNECTIONS_PROTO_PATH).chatplug
-
 export default class GRpcService extends ChatPlugService<GRpcConfig> {
   isEnabled: boolean
-  server: grpc.Server
   name = 'grpc'
 
   connectionsService: ConnectionsService
@@ -26,17 +20,9 @@ export default class GRpcService extends ChatPlugService<GRpcConfig> {
   config: GRpcConfig
 
   async initialize () {
-    this.server = new grpc.Server
-    this.connectionsService.registerService(this.server)
-    this.messagesService.registerService(this.server)
-
-    this.server.bind('0.0.0.0:' + this.config.port, grpc.ServerCredentials.createInsecure())
-    this.server.start()
-
     log.info('grpc', 'GRpc api running under port ' + this.config.port)
   }
 
   async terminate() {
-    await this.server.forceShutdown()
   }
 }
