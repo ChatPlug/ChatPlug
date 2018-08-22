42a109ec39995ddcf7160d9ff2b66c71b7a5743d
alufers <alufers@wp.pl>
2018-08-22 15:25:56 +0200

    Fixed color display

--- src/services/dashboard/web/components/ServiceInstanceList.vue
+++ src/services/dashboard/web/components/ServiceInstanceList.vue
@@ -1,30 +1,30 @@
 <template>
   <div>
     <v-toolbar color="white" flat>
-        <v-btn icon light>
-          <v-icon color="grey darken-2">arrow_back</v-icon>
-        </v-btn>
-        <v-toolbar-title class="grey--text text--darken-4">Service instances</v-toolbar-title>
-        <v-spacer></v-spacer>
+      <v-btn icon light>
+        <v-icon color="grey darken-2">arrow_back</v-icon>
+      </v-btn>
+      <v-toolbar-title class="grey--text text--darken-4">Service instances</v-toolbar-title>
+      <v-spacer></v-spacer>
 
-        <v-btn icon light>
-          <v-icon color="grey darken-2">add</v-icon>
-        </v-btn>
-  </v-toolbar>
+      <v-btn icon light>
+        <v-icon color="grey darken-2">add</v-icon>
+      </v-btn>
+    </v-toolbar>
     <v-list two-line avatar>
       <template v-for="(instance, index) in instances">
 
         <v-divider :key="index + '-divider'"></v-divider>
         <v-list-tile :key="instance.id + '-real-elem'" nuxt :to="`/instances/${instance.id}`">
-          <v-list-tile-avatar v-bind:style="{ color: instance.brandColor || 'red' }">
-            <span class="white--text headline">{{ instance.moduleName[0].toUpperCase() }}</span>
+          <v-list-tile-avatar :style="{ color: instance.serviceModule.brandColor || 'red' }">
+            <span class="headline">{{ instance.serviceModule.displayName[0].toUpperCase() }}</span>
           </v-list-tile-avatar>
           <v-list-tile-content>
             <v-list-tile-title>
               <p>{{ instance.instanceName }}</p>
             </v-list-tile-title>
             <v-list-tile-sub-title>instance of
-              <a>{{ instance.moduleName }}</a>
+              <a>{{ instance.serviceModule.displayName }}</a>
             </v-list-tile-sub-title>
           </v-list-tile-content>
           <v-list-tile-action>
