c398247b7a600e3b9b88a603c6e377229b55dbc2
alufers <alufers@wp.pl>
2018-08-22 14:16:27 +0200

    CS fix

--- src/services/dashboard/web/pages/instances/_id/index.vue
+++ src/services/dashboard/web/pages/instances/_id/index.vue
@@ -3,13 +3,13 @@
 <template>
   <div>
     <v-toolbar color="white" flat>
-        <v-toolbar-title class="grey--text text--darken-4">Service {{id}}</v-toolbar-title>
-        <v-spacer></v-spacer>
+      <v-toolbar-title class="grey--text text--darken-4">Service {{id}}</v-toolbar-title>
+      <v-spacer></v-spacer>
 
-        <v-btn flat color="red">
-          Delete
-        </v-btn>
-  </v-toolbar>
+      <v-btn flat color="red">
+        Delete
+      </v-btn>
+    </v-toolbar>
     <v-tabs slider-color="primary">
       <v-tab ripple>
         Status
@@ -20,7 +20,7 @@
       <v-tab ripple>
         Logs
       </v-tab>
-            <v-tab ripple>
+      <v-tab ripple>
         Threads
       </v-tab>
       <v-tab ripple>
