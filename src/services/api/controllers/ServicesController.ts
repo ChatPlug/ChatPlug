d432e8aa236657323a2861d7cf98e09230d33448
alufers <alufers@wp.pl>
2018-08-22 15:19:29 +0200

    Fixed broken controller & CS fixes

--- src/services/api/controllers/ServicesController.ts
+++ src/services/api/controllers/ServicesController.ts
@@ -1,18 +1,25 @@
-import { JsonController, Get, Param, Post, NotFoundError, Body, BodyParam, BadRequestError, InternalServerError } from 'routing-controllers'
-import ChatPlugContext from '../../../ChatPlugContext'
+import TOML from '@iarna/toml'
 import { classToPlain } from 'class-transformer'
-import Service from '../../../entity/Service'
+import path from 'path'
+import {
+  BadRequestError,
+  BodyParam,
+  Get,
+  InternalServerError,
+  JsonController,
+  NotFoundError,
+  Param,
+  Post,
+} from 'routing-controllers'
+import { Repository } from 'typeorm'
+import ChatPlugContext from '../../../ChatPlugContext'
 import IFieldOptions, {
   fieldListMetadataKey,
   fieldOptionsMetadataKey,
   FieldType,
 } from '../../../configWizard/IFieldOptions'
-import DiscordConfig from '../../discord/DiscordConfig'
-import log from 'npmlog'
-import TOML from '@iarna/toml'
-import fs = require('fs')
-import path from 'path'
-import { Repository } from 'typeorm'
+import Service from '../../../entity/Service'
+import fs from 'fs-extra'
 
 const CONFIG_FOLDER_PATH = path.join(__dirname, '../../../../config')
 @JsonController('/services')
@@ -27,24 +34,34 @@ export default class ServicesController {
   @Get('/instances')
   async getServiceInstances() {
     const instances = await this.servicesRepository.find()
-    const serviceModules = (await this.context.serviceManager.getAvailableServices())
-    return instances.map((el) => el['brandColor'] = serviceModules.find(((mod) => mod.moduleName === el.moduleName)))
+    const serviceModules = await this.context.serviceManager.getAvailableServices()
+    return instances.map(ins => ({
+      ...classToPlain(ins),
+      serviceModule: serviceModules.find(
+        sm => sm.moduleName === ins.moduleName,
+      ),
+    }))
   }
 
   @Post('/instances')
   async createNewInstance(
-    @BodyParam('moduleName', { required: true }) serviceModuleName : string,
-    @BodyParam('instanceName', { required: true }) serviceInstanceName : string,
-    @BodyParam('config', { required: true, parse: true }) configuration : any) {
-    const serviceModule = (await this.context.serviceManager.getAvailableServices())
-      .find((el) => el.moduleName === serviceModuleName)
+    @BodyParam('moduleName', { required: true }) serviceModuleName: string,
+    @BodyParam('instanceName', { required: true }) serviceInstanceName: string,
+    @BodyParam('config', { required: true, parse: true }) configuration: any,
+  ) {
+    const serviceModule = (await this.context.serviceManager.getAvailableServices()).find(
+      el => el.moduleName === serviceModuleName,
+    )
     if (!serviceModule) {
       throw new NotFoundError()
     }
 
     const schema = require(serviceModule.modulePath).Config
-    const fieldList = Reflect.getMetadata(fieldListMetadataKey, new schema()) as string[]
-    if (!fieldList.every((el) => configuration[el] !== undefined)) {
+    const fieldList = Reflect.getMetadata(
+      fieldListMetadataKey,
+      new schema(),
+    ) as string[]
+    if (!fieldList.every(el => configuration[el] !== undefined)) {
       throw new BadRequestError('Config does not match schema')
     }
 
@@ -52,7 +69,10 @@ export default class ServicesController {
       throw new NotFoundError('Service with given name does not exist')
     }
 
-    const instance = await this.servicesRepository.findOne({ moduleName: serviceModuleName, instanceName: serviceInstanceName })
+    const instance = await this.servicesRepository.findOne({
+      moduleName: serviceModuleName,
+      instanceName: serviceInstanceName,
+    })
     if (instance) {
       throw new InternalServerError('Instance with given name already exists')
     }
@@ -60,10 +80,7 @@ export default class ServicesController {
     fs.writeFileSync(
       path.join(
         CONFIG_FOLDER_PATH,
-        serviceModule.moduleName +
-          '.' +
-          serviceInstanceName +
-          '.toml',
+        serviceModule.moduleName + '.' + serviceInstanceName + '.toml',
       ),
       TOML.stringify(configuration),
     )
@@ -84,14 +101,13 @@ export default class ServicesController {
   }
 
   @Get('/instances/:id/disable')
-  async disableService() {
-
-  }
+  async disableService() {}
 
   @Get('/:module/schema')
   async getConfigSchema(@Param('module') moduleName: string) {
-    const serviceModule = (await this.context.serviceManager.getAvailableServices())
-      .find((el) => el.moduleName === moduleName)
+    const serviceModule = (await this.context.serviceManager.getAvailableServices()).find(
+      el => el.moduleName === moduleName,
+    )
     if (!serviceModule) {
       throw new NotFoundError()
     }
@@ -99,14 +115,14 @@ export default class ServicesController {
     const schema = require(serviceModule.modulePath).Config
     const cfg = new schema()
     const fieldList = Reflect.getMetadata(fieldListMetadataKey, cfg) as string[]
-    return fieldList.map((key) => {
-      const options =  classToPlain((Reflect.getMetadata(
+    return fieldList.map(key => {
+      const options = classToPlain(Reflect.getMetadata(
         fieldOptionsMetadataKey,
         cfg,
         key,
-      ) as IFieldOptions))
+      ) as IFieldOptions)
       // @ts-ignore
-      options.type = (FieldType)[options.type]
+      options.type = FieldType[options.type]
       if (options['required'] === undefined) {
         options['required'] = true
       }
@@ -115,7 +131,5 @@ export default class ServicesController {
   }
 
   @Get('/instances/:id/enable')
-  async enableService() {
-
-  }
+  async enableService() {}
 }
