6d4c4d343eafa17a0809a9ff0115299387da1b21
alufers <alufers@wp.pl>
2018-08-24 15:20:57 +0200

    Added basic configuration form

--- src/services/dashboard/web/pages/instances/_id/configuration.vue
+++ src/services/dashboard/web/pages/instances/_id/configuration.vue
@@ -1,20 +1,46 @@
 
 <template>
-    <div>
-        Configuration
-    </div>
+  <v-container>
+
+    <template v-for="configField in configSchema">
+      <v-layout :key="configField.name">
+        <v-text-field v-if="configField.type === 'STRING'" :label="configField.name" :hint="configField.hint"></v-text-field>
+        <v-text-field v-else-if="configField.type === 'NUMBER'" :label="configField.name" :hint="configField.hint" type="number"></v-text-field>
+        <v-checkbox v-else-if="configField.type === 'BOOLEAN'" :label="configField.name" />
+        <v-tooltip left>
+          <v-btn flat icon color="grey darken-1" slot="activator">
+            <v-icon>info</v-icon>
+          </v-btn>
+          <span>{{configField.hint}}</span>
+        </v-tooltip>
+      </v-layout>
+    </template>
+
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'nuxt-property-decorator'
 import { State, namespace, Action } from 'vuex-class'
+import * as actions from '../../../store/modules/services/actions.types'
+import ServiceInstance from '../../../types/ServiceInstance'
 
-@Component({
-  name: 'dupa-123389',
-})
+const servicesModule = namespace('services')
+
+@Component({})
 export default class extends Vue {
-  @Prop() currentInstance
-  created() {}
+  @Prop() currentInstance: ServiceInstance
+  @servicesModule.Action(actions.LOAD_INSTANCE_CONFIG_SCHEMA)
+  loadInstanceConfigSchema
+  created() {
+    this.loadInstanceConfigSchema({ id: this.currentInstance.id })
+  }
+  get configSchema() {
+    if (!this.currentInstance) {
+      return null
+    }
+    return this.currentInstance.serviceModule.configSchema || null
+  }
 }
 </script>
 <style scoped>
--- src/services/dashboard/web/store/modules/services/actions.ts
+++ src/services/dashboard/web/store/modules/services/actions.ts
@@ -3,10 +3,24 @@ import { ActionTree } from 'vuex'
 import axios from '~/axios'
 import * as action from './actions.types'
 import ServicesState from './ServicesState'
+import encodeParams from '../../../encodeParams'
 
 export default <ActionTree<ServicesState, {}>>{
   async [action.LOAD_INSTANCES](store) {
     const { data } = await axios.get('services/instances')
     store.commit(action.SET_INSTANCES, data.data)
+  },
+  async [action.LOAD_INSTANCE_CONFIG_SCHEMA](store, { id }: { id: number }) {
+    const instance = store.state.instances.find(i => i.id === id)
+    if (!instance) {
+      throw new Error(`Failed to find instance with id ${id}`)
+    }
+    const { data } = await axios.get(
+      encodeParams`services/${instance.serviceModule.moduleName}/schema`,
+    )
+    store.commit(action.SET_INSTANCE_CONFIG_SCHEMA, {
+      id,
+      configSchema: data.data,
+    })
   },
 }
