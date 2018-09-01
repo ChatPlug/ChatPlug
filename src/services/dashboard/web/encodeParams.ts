8671cd8800470c32f5f6bc9006d0db121f7904a1
Unknown <kacper.potyrala@gmail.com>
2018-09-01 21:04:32 +0200

    Fix errors

--- src/services/dashboard/web/encodeParams.ts
+++ src/services/dashboard/web/encodeParams.ts
@@ -2,7 +2,7 @@
  * A function for tagged template literals that encodes params for URLs.
  */
 export default function encodeParams(template, ...expressions) {
-    return template.reduce((a, part, i) => {
-        return a + encodeURIComponent(expressions[i - 1]) + part;
-    });
+  return template.reduce((a, part, i) => {
+    return a + encodeURIComponent(expressions[i - 1]) + part
+  })
 }
