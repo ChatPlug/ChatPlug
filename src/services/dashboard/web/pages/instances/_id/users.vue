6cee7b5c32b406ea3610da57c08f31b30c77e6f3
SerekKiri <kacper.potyrala@gmail.com>
2018-09-08 18:26:19 +0200

    feat: Show users

--- src/services/dashboard/web/pages/instances/_id/users.vue
+++ src/services/dashboard/web/pages/instances/_id/users.vue
@@ -1,19 +1,20 @@
-
 <template>
-      <v-list>
-           <v-list-tile
-            v-for="user in users"
-            :key="user.username"
-            avatar
-          >
-          <v-list-tile-content>
-              <v-list-tile-title v-text="user.username"></v-list-tile-title>
-            </v-list-tile-content>
+  <v-list two-line avatar>
+      <template v-for="(user, index) in users">
 
-            <v-list-tile-avatar>
+        <v-divider :key="index+ '-divider'"></v-divider>
+        <v-list-tile :key="user.id + '-real-elem'">
+          <v-list-tile-content>
+            <v-list-tile-title  v-text="user.username">
+            </v-list-tile-title>
+            <v-list-tile-sub-title  v-text="'#' + user.externalServiceId">
+            </v-list-tile-sub-title>
+          </v-list-tile-content>
+           <v-list-tile-avatar>
               <img :src="user.avatarUrl">
             </v-list-tile-avatar>
-          </v-list-tile>
+        </v-list-tile>
+      </template>
     </v-list>
 </template>
 
@@ -29,17 +30,18 @@ export default class extends Vue {
   @Prop() currentInstance
   @servicesModule.Action(actions.LOAD_USERS) loadUsers
 
+
   async created() {
     this.loadUsers({ id: this.currentInstance.id })
   }
 
-  get users() {
+  /*get users() {
     if (!this.currentInstance) {
       return null
     }
 
     return this.currentInstance.users || null
-  }
+  }*/
 }
 </script>
 <style scoped>
