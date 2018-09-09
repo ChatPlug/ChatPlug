1eb8041eca1494fb654d4f4743e47f35db19b71f
SerekKiri <kacper.potyrala@gmail.com>
2018-09-09 14:19:28 +0200

    First Threads view

--- src/services/dashboard/web/pages/instances/_id/threads.vue
+++ src/services/dashboard/web/pages/instances/_id/threads.vue
@@ -1,16 +1,32 @@
 
 <template>
-    <div>
-        Thereads
-    </div>
+    <v-list two-list>
+      <template v-for="(thread, index) in threads">
+        <v-divider :key="index+ '-divider'"></v-divider>
+        <v-list-tile :key="thread.id + '-real-elem'">
+          <v-list-tile-content>
+            <v-list-tile-title  v-text="thread.id">
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </template>
+    </v-list>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
 import { State, namespace, Action } from 'vuex-class'
+import * as actions from '../../../store/modules/services/actions.types'
 
-@Component({})
-export default class extends Vue {}
+const servicesModule = namespace('services')
+ @Component({})
+export default class extends Vue {
+  @Prop() currentInstance
+  @servicesModule.Action(actions.LOAD_INSTANCE_THREAD) loadThreads
+   async created() {
+      this.loadThreads()
+  }
+}
 </script>
 <style scoped>
 </style>
