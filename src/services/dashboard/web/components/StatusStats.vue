bb8849be4b7b6086c80114ecfc77cbab48e70b90
SerekKiri <kacper.potyrala@gmail.com>
2018-09-27 15:02:59 +0200

    feat: Add icons

--- src/services/dashboard/web/components/StatusStats.vue
+++ src/services/dashboard/web/components/StatusStats.vue
@@ -4,7 +4,12 @@
       <v-card color="indigo accent-2" class="white--text">
         <v-card-title primary-title>
           <div>
-            <div class="headline">2221</div>
+           
+            <div class="headline">2221
+                <div class="icons">
+                <v-icon class="white--text">message</v-icon>
+                </div>
+              </div>
             <span class="white--text">Messages sent today</span>
           </div>
         </v-card-title>
@@ -15,6 +20,9 @@
         <v-card-title primary-title>
           <div>
             <div class="headline">4021</div>
+                <div class="icons">
+                <v-icon class="white--text">text sms</v-icon>
+                </div>
             <span class="white--text">Messages sent this week</span>
           </div>
         </v-card-title>
@@ -25,6 +33,9 @@
         <v-card-title primary-title>
           <div>
             <div class="headline">516</div>
+                <div class="icons">
+                <v-icon class="white--text">people</v-icon>
+                </div>
             <span class="white--text">Friends connected</span>
           </div>
         </v-card-title>
@@ -35,6 +46,9 @@
         <v-card-title primary-title>
           <div>
             <div class="headline">21</div>
+                <div class="icons">
+                <v-icon class="white--text">forum</v-icon>
+                </div>
             <span class="white--text">Connected conversations</span>
           </div>
         </v-card-title>
@@ -58,3 +72,12 @@ export default class StatusStats extends Vue {
 
 }
 </script>
+
+<style lang="css" scoped>
+    .icons {
+       float: right;
+       position: absolute;
+       top: 1rem;
+       right: 1rem;
+    }
+</style>
