ab2cbdc62245226b5e59f1c4a5126266aef0af52
Filip Grzywok <rj23@protonmail.com>
2018-10-02 22:29:50 +0200

    migrated facebook service to libfb

--- src/services/facebook/FacebookService.ts
+++ src/services/facebook/FacebookService.ts
@@ -1,22 +1,13 @@
 import log from 'npmlog'
 import { IChatPlugMessage, IChatPlugThread, IChatPlugThreadResult } from '../../models'
 import { ChatPlugService } from '../Service'
-import { Subject } from 'rxjs'
-// import { FacebookConfig } from './FacebookConfig'
-import facebook from 'facebook-chat-api'
-import { createInterface } from 'readline'
-import { ExchangeManager } from '../../ExchangeManager'
-import { ThreadConnectionsManager } from '../../ThreadConnectionsManager'
+import fs from 'fs'
 import { FacebookMessageHandler } from './FacebookMessageHandler'
 import { ChatPlugConfig } from '../../ChatPlugConfig'
 import Message from '../../entity/Message'
+import { login } from 'libfb'
 import FacebookConfig from './FacebookConfig'
 import { LogLevel } from '../../Logger'
-
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
 
 export default class FacebookService extends ChatPlugService<FacebookConfig> {
   messageHandler: FacebookMessageHandler
@@ -31,79 +22,56 @@ export default class FacebookService extends ChatPlugService<FacebookConfig> {
 
     this.receiveMessageSubject.subscribe(this.messageHandler.onIncomingMessage)
 
-    this.stopListening = this.facebook.listen(this.listener)
+    this.stopListening = this.facebook.on('message', (message) => {
+      this.messageHandler.onOutgoingMessage(message)
+    })
   }
 
   listener = async (err, message) => {
     if (err) return log.error('facebook', err)
-
-    // Reconnecting
-    if (!message) {
-      this.log(LogLevel.WARN, 'Message is undefined/null, internet connection may be unavailable')
-      this.log(LogLevel.INFO, 'Reconnecting')
-
-      this.stopListening()
-      await this.login()
-      this.log(LogLevel.INFO, 'Reconnected to facebook')
-      this.messageHandler.setClient(this.facebook)
-      this.stopListening = this.facebook.listen(this.listener)
-    }
     this.messageHandler.onOutgoingMessage(message)
   }
 
   async getThreads() {
-    return new Promise<IChatPlugThreadResult[]>((res, rej) => {
-      this.facebook.getThreadList(50, null, ['INBOX'], (err, list) => {
-
-        res(list.filter((el) => el.name !== null).map((el) => {
-          let subtitle = 'Group'
-          if (!el.isGroup) {
-            subtitle = 'Conversation'
-          }
-          return {
-            subtitle,
-            title: el.name,
-            id: el.threadID,
-          } as IChatPlugThreadResult
-        }))
-      })
+    const threads = await this.facebook.getThreadList(500)
+    return threads.filter((el) => el.name !== null).map((el) => {
+      let subtitle = 'Group'
+      if (!el.isGroup) {
+        subtitle = 'Conversation'
+      }
+      return {
+        subtitle,
+        title: el.name,
+        id: el.id.toFixed(),
+      } as IChatPlugThreadResult
     })
   }
 
-  login() {
-    return new Promise((resolve, reject) => {
-      facebook(
-        {
-          email: this.config.email,
-          password: this.config.password,
-        },
-        {
-          forceLogin: this.config.forceLogin,
-          logLevel: process.env.NODE_ENV === 'development' ? 'info' : 'warn',
-        },
-        async (err, api) => {
-          if (err) {
-            if (err.error !== 'login-approval') return reject(err)
-            this.context.logger.log(this.dbService, LogLevel.INFO, 'Login approval pending...')
-            const message =
-              'Enter login approval code to your Facebook account (SMS or Google Authenticator app): '
-            const code = await new Promise(result =>
-              rl.question(message, result),
-            )
-            return err.continue(code)
-          }
-          this.facebook = api
-          this.log(LogLevel.INFO, 'Logged in as ' + this.config.email)
-          resolve()
-        },
-      )
-    })
+  async login() {
+    let session = null
+    if (fs.existsSync('session.json')) {
+      session = JSON.parse(fs.readFileSync('session.json', 'utf8'))
+    }
+
+    if (session) {
+      this.facebook = await login(this.config.email, this.config.password, { session, selfListen: true })
+    } else {
+      this.facebook = await login(this.config.email, this.config.password, { selfListen: true })
+    }
+
+    setTimeout(
+      () => {
+        fs.writeFileSync('session.json', JSON.stringify(this.facebook.getSession()))
+        this.log(LogLevel.INFO, 'Logged in as ' + this.facebook.getSession().identifier)
+      },
+      15000)
+
   }
 
   async terminate() {
     if (!this.facebook) return this.log(LogLevel.INFO, 'Not logged in')
 
-    await this.facebook.logout
+    // await this.facebook.logout
   }
 
   async searchThreads(query: string): Promise<IChatPlugThreadResult[]> {
