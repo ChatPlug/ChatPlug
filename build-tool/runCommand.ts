8098a5e9bab9a046cfc8fde178f55a61b722aa8f
alufers <alufers@wp.pl>
2018-10-30 16:42:31 +0100

    Added better error handling to runcOmmand in build tool

--- build-tool/runCommand.ts
+++ build-tool/runCommand.ts
@@ -4,19 +4,16 @@ import fs from 'fs-extra'
 
 export default async function runCommand(command: string, cwd?: string) {
   await new Promise((res, rej) => {
-    let nuxtProc = exec(
-     command,
-      {
-        cwd
-      },
-    )
+    let nuxtProc = exec(command, {
+      cwd,
+    })
     nuxtProc.stdout.pipe(process.stdout)
     nuxtProc.stderr.pipe(process.stderr)
     nuxtProc.on('exit', code => {
       if (code === 0) {
         res()
       } else {
-        rej()
+        rej(new Error(`Running '${command}' failed with error code ${code}`))
       }
     })
   })
