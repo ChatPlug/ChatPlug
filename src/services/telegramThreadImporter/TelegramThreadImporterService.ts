33ef3149928fe6bc82011cb92105145118ff7521
alufers <alufers@wp.pl>
2018-10-30 21:44:51 +0100

    Trying to fix build on windows, removed tdl and added webhooks

--- src/services/telegramThreadImporter/TelegramThreadImporterService.ts
+++ src/services/telegramThreadImporter/TelegramThreadImporterService.ts
@@ -6,57 +6,59 @@ import TelegramThreadImporterConfig from './TelegramThreadImporterConfig'
 import { ExchangeManager } from '../../ExchangeManager'
 import { ThreadConnectionsManager } from '../../ThreadConnectionsManager'
 import { ChatPlugConfig } from '../../ChatPlugConfig'
-import { Client } from 'tdl'
+//import { Client } from 'tdl'
 
-export default class TelegramThreadImporterService extends ChatPlugService<TelegramThreadImporterConfig> {
+export default class TelegramThreadImporterService extends ChatPlugService<
+  TelegramThreadImporterConfig
+> {
   messageSubject: Subject<IChatPlugMessage>
   receiveMessageSubject: Subject<MessagePacket> = new Subject()
   config: TelegramThreadImporterConfig
-  client: Client
+  client: any // Client
   coreConfig: ChatPlugConfig
 
   async initialize() {
-    this.client = new Client({
-      apiId: Number(this.config.apiId),
-      apiHash: this.config.apiHash,
-      loginDetails: {
-        phoneNumber: this.config.phoneNumber,
-      },
-    })
+    // this.client = new Client({
+    //   apiId: Number(this.config.apiId),
+    //   apiHash: this.config.apiHash,
+    //   loginDetails: {
+    //     phoneNumber: this.config.phoneNumber,
+    //   },
+    // })
 
-    await this.client.connect()
+    // await this.client.connect()
 
-    this.receiveMessageSubject.subscribe(
-      async (msg: MessagePacket) => {
-        /*if (msg.origin.service !== 'telegram') {
-          const user = await this.client.invoke({
-            _: 'getUser',
-            user_id: '612705604',
-          } as any)
+    // this.receiveMessageSubject.subscribe(
+    //   async (msg: MessagePacket) => {
+    //     /*if (msg.origin.service !== 'telegram') {
+    //       const user = await this.client.invoke({
+    //         _: 'getUser',
+    //         user_id: '612705604',
+    //       } as any)
 
-          const result = await this.client.invoke({
-            _: 'createNewBasicGroupChat',
-            title: msg.origin.name,
-            user_ids: ['612705604'],
-          } as any) as any
+    //       const result = await this.client.invoke({
+    //         _: 'createNewBasicGroupChat',
+    //         title: msg.origin.name,
+    //         user_ids: ['612705604'],
+    //       } as any) as any
 
-          const newThread = {
-            service: 'telegram',
-            name: msg.origin.name,
-            id: result.id.toString(),
-          }
+    //       const newThread = {
+    //         service: 'telegram',
+    //         name: msg.origin.name,
+    //         id: result.id.toString(),
+    //       }
 
-          /*this.coreConfig.addThreadConnection({
-           services: [newThread, msg.origin],
-          })*/
+    //       /*this.coreConfig.addThreadConnection({
+    //        services: [newThread, msg.origin],
+    //       })*/
 
-          // msg.target = newThread
-          // this.messageSubject.next(msg)
-      })
+    //       // msg.target = newThread
+    //       // this.messageSubject.next(msg)
+    //   })
     log.info('telegram', 'Registered bot handlers')
   }
 
   async terminate() {
-    await this.client.destroy()
+    //await this.client.destroy()
   }
 }
