d56d858c06433e71001745cc6017d53cc348fe58
alufers <alufers@wp.pl>
2018-10-30 20:50:56 +0100

    Added building on diffrent platforms in preparation for embedding native modules

--- build-tool/packageApp.ts
+++ build-tool/packageApp.ts
@@ -1,14 +1,17 @@
 import fs from 'fs-extra'
 import { exec as pkg } from 'pkg'
 import path from 'path'
-
 import runCommand from './runCommand'
 import { flag } from './buildTool'
 import loggingHelper from './loggingHelper'
+import os from 'os'
 
 export default async function packageApp(usedModules: string[]) {
   usedModules.push('sqlite3') // include sqlite3 even if it isn't directly required
+
+  // prepare the binaries directory
   await fs.mkdirp(path.resolve(__dirname, '../dist-bin'))
+
   const fullPackage = await fs.readJSON(
     path.resolve(__dirname, '../package.json'),
   )
@@ -16,13 +19,17 @@ export default async function packageApp(usedModules: string[]) {
     ...fullPackage.dependencies,
     ...fullPackage.devDependencies,
   }
-  const depndenciesToPackage = {}
+  const depndenciesToPackage: { [x: string]: string } = {}
+
+  // only copy packages used by the app directly
   Object.keys(allDependencies)
     .filter(dep => usedModules.includes(dep))
     .forEach(dep => (depndenciesToPackage[dep] = allDependencies[dep]))
   const packagePath = path.resolve(__dirname, '../dist/package.json')
+  // save the new package with some extra data for PKG
   await fs.writeJSON(packagePath, {
-    name: 'ChatPlug',
+    ...fullPackage,
+    scripts: undefined, // unset scripts, useless in built app
     dependencies: depndenciesToPackage,
     bin: 'chatplug.js',
     pkg: {
@@ -45,5 +52,26 @@ export default async function packageApp(usedModules: string[]) {
     )
   }
   console.log({ packagePath })
-  await pkg([packagePath, '--output', path.resolve(__dirname, `../dist-bin/ChatPlug-${process.env.TRAVIS_TAG || 'dev'}`), '--target', 'linux,macos,win'])
+
+  const targetMatrix: { [key in NodeJS.Platform]?: string } = {
+    darwin: 'macos',
+    win32: 'win',
+    linux: 'linux-x64,linux-x86,linux-armv6,linux-armv7,',
+  }
+  const target = targetMatrix[os.platform()]
+
+  if (!target) {
+    throw new Error(`Unsupported build platform ${os.platform()}.`)
+  }
+
+  await pkg([
+    packagePath,
+    '--output',
+    path.resolve(
+      __dirname,
+      `../dist-bin/chatplug-${process.env.TRAVIS_TAG || 'dev'}`,
+    ),
+    '--target',
+    target,
+  ])
 }
