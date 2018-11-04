4abcf6b5cf93a93acf281c71a3daf8fd6eaec194
alufers <alufers@wp.pl>
2018-11-04 13:32:40 +0100

    Refactored the logger a little bit

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,7 +1,7 @@
-import consola from 'consola'
 import ChatPlugContext from './ChatPlugContext'
 import Log from './entity/Log'
 import Service from './entity/Service'
+import chalk from 'chalk'
 
 export enum LogLevel {
   ERROR = 'error',
@@ -11,21 +11,48 @@ export enum LogLevel {
   FATAL = 'fatal',
 }
 
+type LevelMap = { [x in LogLevel]: string }
+
+const levelBadges: LevelMap = {
+  error: chalk.bgRed.white(' ERROR '),
+  info: chalk.bgBlue.black(' INFO '),
+  warn: chalk.bgYellow.black(' WARN '),
+  fatal: chalk.bgWhite.bgRed(' FATAL '),
+  debug: chalk.bgCyan.bgGreen(' DEBUG '),
+}
+
+const levelArrows: LevelMap = {
+  error: chalk.red('›'),
+  info: chalk.blue('›'),
+  warn: chalk.yellow('›'),
+  fatal: chalk.white('›'),
+  debug: chalk.cyan('›'),
+}
+
 export default class Logger {
   constructor(public context: ChatPlugContext, public service?: Service) {}
 
   async log(logLevel: LogLevel, msg: string) {
+    const scope = this.service ? this.service.moduleName : 'core'
+    console.log(
+      levelBadges[logLevel],
+      chalk.gray(
+        new Date()
+          .toISOString()
+          .replace('T', ' ')
+          .substr(0, 19),
+      ),
+      chalk.reset(scope),
+      levelArrows[logLevel],
+      chalk.reset(msg),
+    )
+
     const log = new Log()
     log.logLevel = logLevel
     log.systemLog = false
     log.message = msg
     log.service = this.service
-    this.context.connection.getRepository(Log).save(log)
-    consola[logLevel]({
-      message: msg,
-      badge: true,
-      scope: this.service ? this.service.moduleName : 'core',
-    })
+    await this.context.connection.getRepository(Log).save(log)
   }
 
   error(msg: string) {
